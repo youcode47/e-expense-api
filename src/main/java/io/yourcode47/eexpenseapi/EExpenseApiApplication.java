2a5c31e6ccecd09f6b11b7fcc1986a99f9cc48ce
Ange Carmel YORO <ange.yoro@adservio.fr>
2022-02-10 15:18:35 +0100

    first commit

---
+++ src/main/java/io/yourcode47/eexpenseapi/EExpenseApiApplication.java
@@ -0,0 +1,13 @@
+package io.yourcode47.eexpenseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EExpenseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EExpenseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/yourcode47/eexpenseapi/EExpenseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yourcode47.eexpenseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EExpenseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
