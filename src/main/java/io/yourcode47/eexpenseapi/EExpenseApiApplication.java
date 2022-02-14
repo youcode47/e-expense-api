636de908b404fac13a2ef0dd01b90017fe4b247c
Ange Carmel YORO <ange.yoro@adservio.fr>
2022-02-14 12:29:41 +0100

    Edit 7

--- src/main/java/io/yourcode47/eexpenseapi/EExpenseApiApplication.java
+++ src/main/java/io/yourcode47/eexpenseapi/EExpenseApiApplication.java
@@ -2,8 +2,6 @@ package io.yourcode47.eexpenseapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @SpringBootApplication
 public class EExpenseApiApplication {
